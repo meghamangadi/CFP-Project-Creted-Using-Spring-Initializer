af6d802deb8b4d31aa06db5ab632c78024395af9
megha angadi <meghamangadi@gmail.com>
2021-11-06 12:37:16 +0530

    Added: Created a Project to learn Spring Concepts using Spring Initializer

--- src/main/java/com/bridgeLabz/demo/HelloWorldApplication.java
+++ src/main/java/com/bridgeLabz/demo/HelloWorldApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Project Created Using Spring Initializer");
 		SpringApplication.run(HelloWorldApplication.class, args);
+		 
+		
+		
 	}
 
 }

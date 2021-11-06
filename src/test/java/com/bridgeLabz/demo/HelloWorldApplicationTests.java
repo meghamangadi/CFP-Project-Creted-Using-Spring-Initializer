08ee658026e9b7a80c0ff752a50ac4ecefde00bb
megha angadi <meghamangadi@gmail.com>
2021-11-06 12:30:58 +0530

    Added: Created a Project to learn Spring Concepts

---
+++ src/test/java/com/bridgeLabz/demo/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgeLabz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
